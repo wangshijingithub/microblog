eaa6c8356cd1126f2b0af30beac1f63448cb7d39
wangshijin <275201600@qq.com>
2016-11-06 21:56:04 +0800

    sunday

--- app/views.py
+++ app/views.py
@@ -1,23 +1,35 @@
 ﻿# microblog/app/
 # -*- coding:utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-	user = {'nickname': 'Jordon'}
-	posts = [
-		{
-			'author': {'nickname': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'nickname': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-		]
-	return render_template('index.html',
-							title='Home',
-							user=user,
-							posts=posts)
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+			(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',
+							title='Sign In',
+							form=form)
+# @app.route('/index')
+# def index():
+# 	user = {'nickname': 'Jordon'}
+# 	posts = [
+# 		{
+# 			'author': {'nickname': 'John'},
+# 			'body': 'Beautiful day in Portland!'
+# 		},
+# 		{
+# 			'author': {'nickname': 'Susan'},
+# 			'body': 'The Avengers movie was so cool!'
+# 		}
+# 		]
+# 	return render_template('index.html',
+# 							title='Home',
+# 							user=user,
+# 							posts=posts)
